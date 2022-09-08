31448ca114d9f4c258c5f0ad038fa27d0a0e5e1e
Mark <shawsi1988@gmail.com>
2022-09-08 10:36:14 +0100

    helper function to flatten products object created

---
+++ db/helpers/dataFlattener.js
@@ -0,0 +1,21 @@
+const products = require('../data/products.json');
+
+function flattenObject(products) {
+    let flatProducts = {};
+
+    for (let i in products) {
+        if (!products.hasOwnProperty(i)) continue;
+
+        if ((typeof products[i]) == 'object' && products[i] !== null) {
+            let flatObject = flattenObject(products[i]);
+            for (let x in flatObject) {
+                if (!flatObject.hasOwnProperty(x)) continue;
+
+                toReturn[i + '.' + x] = flatObject[x];
+            }
+        } else {
+            flatProducts[i] = ob[i];
+        }
+    }
+    return flatProducts;
+}
