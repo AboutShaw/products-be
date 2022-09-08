1674a916f857f5f163a4882f3606e760e50b85e1
Mark <shawsi1988@gmail.com>
2022-09-08 10:51:30 +0100

    typesArray function created to return array of unique products types

---
+++ db/helpers/typesArray.js
@@ -0,0 +1,13 @@
+const products = require('../data/products.json');
+
+function typesFromProducts(products) {
+    let productTypes = [];
+
+    for(let i in products) {
+        productTypes.push(i.type)
+    }
+
+    productTypes = Set(productTypes);
+
+    return productTypes;
+}
