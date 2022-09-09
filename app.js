1d6992761e10f1f5ddca12a4fbc74923c940a347
AboutShaw <shawsi1988@gmail.com>
2022-09-09 14:17:18 +0100

    error handling added

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
-let flattenObject = require('./db/helpers')
+const cors = require('cors');
+const express = require("express");
 
-console.log(flattenObject);
+const {
+  getProducts
+} = require("./controllers/index");
+
+const {
+    handleCustomErrors,
+    handlePsqlErrors,
+    handleServerErrors,
+  } = require('./errors/index.js');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+// Requires and util functions above this line -----
+// API requests below this line -----
+app.get(`/api/products`, getProducts);
+
+
+
+// Error handling below this line -----
+app.use(handleCustomErrors);
+app.use(handlePsqlErrors);
+app.use(handleServerErrors);
+
+app.all('/*', (req, res) => {
+  res.status(404).send({ msg: 'Route not found' });
+});
+
+module.exports = app;
