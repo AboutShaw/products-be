7c062d4f2d8b05c148459a9ed8cc0e9c43d9f7fa
AboutShaw <shawsi1988@gmail.com>
2022-09-09 19:52:38 +0100

    option to change price order in selectProducts API

--- models/index.js
+++ models/index.js
@@ -1,12 +1,13 @@
 const db = require("../db");
 const { typesFromProducts } = require("../db/helpers/utils");
 
-exports.selectProducts = () => {
+exports.selectProducts = ( {order = 'ASC', limit = 100}) => {
     return db
       .query(
         `    SELECT  *
                   FROM    products
-                  ORDER BY priceValue ASC;`
+                  ORDER BY priceValue ${order}
+                  LIMIT ${limit};`
       )
       .then((result) => {
         return result.rows;
