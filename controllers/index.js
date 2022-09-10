36efac068eef92d8e8683899e52d770912c5b08d
AboutShaw <shawsi1988@gmail.com>
2022-09-10 13:20:22 +0100

    latest working be

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,24 @@
+const Joi = require('joi');
 const {
     selectProducts
   } = require("../models/index");
 
 const apis = require(`../endpoints.json`);
 
+const schema = Joi.object({
+  sort: Joi.string()
+    .alphanum()
+    .valid('asc', 'desc'),
+  password: Joi.number()
+    .integer()
+    .min(1)
+    .max(100),
+})
+
 exports.getProducts = (req, res, next) => {
-  const { sort, limit } = req.params;
+  let { sort, limit } = req.params
+
+  schema.validate({ sort, limit })
 
   selectProducts({ sort, limit }).then((products) => {
     res.status(200).send({ products })})
